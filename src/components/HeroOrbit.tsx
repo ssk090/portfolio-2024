d5a1e2b2bcf53f5bbe5c30ed1310638d8affaebb
ssk090 <shivanandasai.38@gmail.com>
2024-08-27 01:29:37 +0530

    checkpoint hero section done

---
+++ src/components/HeroOrbit.tsx
@@ -0,0 +1,30 @@
+import StarIcon from "@/assets/icons/star.svg";
+import { PropsWithChildren } from "react";
+
+export const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div
+        className="flex items-start justify-start animate-spin [animation-duration:45s]"
+        style={{
+          transform: `rotate(${rotation}deg)`,
+          height: `${size}px`,
+          width: `${size}px`,
+        }}
+      >
+        <div
+          className="inline-flex animate-spin [animation-duration:5s]"
+          style={{
+            transform: `rotate(${rotation * -1}deg)`,
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,16 +1,64 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
+import { HeroOrbit } from "@/components/HeroOrbit";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{
+            backgroundImage: `url(${grainImage.src})`,
+          }}
+        ></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+
+        <HeroOrbit size={800} rotation={-72}>
+          <StarIcon className="size-28 text-white" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <StarIcon className="size-12 text-white" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-white" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-14}>
+          <SparkleIcon className="size-8 text-white/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="size-5 text-white/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178}>
+          <SparkleIcon className="size-10 text-white/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="size-14 text-white/20" />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={85}>
+          <div className="size-3 bg-yellow-300 rounded-full"></div>
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-41}>
+          <div className="size-2 bg-yellow-300 rounded-full"></div>
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5}>
+          <div className="size-3 bg-yellow-300 rounded-full"></div>
+        </HeroOrbit>
+      </div>
+
       <div className="container">
         <div className="flex flex-col items-center justify-center">
           <Image src={memojiImage} alt="Memoji Computer" width={100} />
           <div className="bg-slate-950 border border-gray-800 px-4 py-2 rounded-3xl text-white/90 text-sm inline-flex items-center gap-4">
             <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">Coding in VS Code</div>
+            <div className="text-sm font-medium">Caffine and Code</div>
           </div>
         </div>
         <div className="">
