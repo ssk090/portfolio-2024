50617b326cf59e93add923b5f19fb11401c4fd77
ssk090 <shivanandasai.38@gmail.com>
2024-08-31 01:44:15 +0530

    fix buttons

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -86,7 +86,11 @@ export const ProjectsSection = () => {
                       </li>
                     ))}
                   </ul>
-                  <a href={project.link}>
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 ">
                       <span>Visit Live Site</span>
                       <ArrowUpRight className="size-5" />
